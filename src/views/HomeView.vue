fd80924ff473161acfa14db77d8e7a7f76c86791
DeeGeeWest <DeeGeeWest@gmail.com>
2022-04-08 17:21:03 -0400

    show with MODAl

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,75 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      contacts: [],
+      createNew: {},
+      currentContact: {},
+    };
+  },
+  created: function () {
+    this.contactIndex();
+  },
+  methods: {
+    contactIndex() {
+      axios.get("/contacts").then((response) => {
+        console.log(response.data);
+        this.contacts = response.data;
+      });
+    },
+    createContact() {
+      axios.post("/contacts", this.createNew).then((response) => {
+        console.log(response.data);
+        this.contacts.push(response.data);
+      });
+    },
+    showContact(contact) {
+      this.currentContact = contact;
+      document.querySelector("#contact-details").showModal();
+      console.log(contact);
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <p>{{ contacts.length }}</p>
+    <h2>New Contact</h2>
+    First
+    <input type="text" v-model="createNew.first_name" />
+    Last
+    <input type="text" v-model="createNew.last_name" />
+    Email
+    <input type="text" v-model="createNew.email" />
+    Image
+    <input type="text" v-model="createNew.image" />
+    <button v-on:click="createContact()">Create</button>
+    <div v-for="contact in contacts" v-bind:key="contact.id">
+      <h3>{{ contact.first_name }} {{ contact.last_name }}</h3>
+      <h4>{{ contact.email }}</h4>
+      <p>{{ contact.phone_number }}</p>
+      <img v-bind:src="contact.image" v-bind:alt="contact.name" style="max-width: 250px" />
+      <br />
+      <div>
+        <button v-on:click="showContact(contact)">More Info</button>
+      </div>
+    </div>
+    <dialog id="contact-details">
+      <form method="dialog">
+        <h1>Contact Info</h1>
+        <p>First Name: {{ currentContact.first_name }}</p>
+        <p>Last Name: {{ currentContact.last_name }}</p>
+        <p>Email: {{ currentContact.email }}</p>
+        <img v-bind:src="currentContact.image" v-bind:alt="currentContact.first_name" style="max-width: 250px" />
+        <button>CLOSE</button>
+      </form>
+    </dialog>
+  </div>
+</template>
+
+<style></style>
